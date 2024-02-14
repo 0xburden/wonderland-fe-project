d7e5d602b8db2e6cf5676e23d6e857e5140a3cc1
0xburden <0xburden@protonmail.com>
2024-02-13 20:01:13 -0800

    add/change some redux boilerplate

--- src/modules/redux/store.ts
+++ src/modules/redux/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { tokenReducer } from './slices'
+import { tokenReducer } from './slices/token'
 
 export const store = configureStore({
   reducer: {
     token: tokenReducer,
   },
 })
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
